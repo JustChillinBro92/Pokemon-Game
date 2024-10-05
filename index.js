d478fae983b526403e2e8dcd88521cf1b1faa039
JustChillinBro92 <ayanpurkait11@gmail.com>
2024-10-06 04:12:09 +0530

    player & background position setup

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image()
-image.src = './img/Petalwood Town.png'  //html element i.e. the image(map)
+const image = new Image();
+image.src = "./img/Petalwood Town.png"; //html element i.e. the image(map)
 
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
 
-image.onload = () => {    // map needs to be loaded first before drawing
-c.drawImage(image, -220, 0)  //needs html element to be referenced
-c.drawImage(playerImage, canvas.width/2 - playerImage.width/2 , canvas.height/2 - playerImage.height/2)  //needs html element to be referenced
+image.onload = () => {
+  // map needs to be loaded first before drawing
+  //needs html element to be referenced
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.drawImage(image, -199, 0); //needs html element to be referenced
+
+  // Scale the player image
+  const playerWidth = playerImage.width * 0.8; // Scale down by 80% (adjust this value as needed)
+  const playerHeight = playerImage.height * 0.8;
+
+  c.drawImage(
+    playerImage,
+    0, //cropping the sprite-sheet of the player
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 1.26,
+    canvas.height / 2 - playerImage.height / 2 - 28,
+    playerWidth / 4, //actual height and width of player being rendered out
+    playerHeight
+  );
 }
+animate();
 
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      console.log("pressed w key");
+      break;
 
+    case "a":
+      console.log("pressed a key");
+      break;
 
+    case "s":
+      console.log("pressed s key");
+      break;
+
+    case "d":
+      console.log("pressed d key");
+      break;
+  }
+});
