0a8e86c39bc7a8363e3dc606cca1973b1c71efe2
JustChillinBro92 <ayanpurkait11@gmail.com>
2024-10-07 22:43:25 +0530

    collisions WIP

--- index.js
+++ index.js
@@ -6,8 +6,48 @@ console.log(collisions);
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const collisionsMap = [];
+for (let i = 0; i <= collisions.length; i += 120) {
+  collisionsMap.push(collisions.slice(i, 120 + i)); //slicing the array of collisons(acc to width of map 120 tiles) into sub-arrays and storing/pushing them in another Array
+}
+//console.log(collisionsMap)
+
+class Boundary {
+  static width = 45.6;
+  static height = 45.6;
+
+  constructor({ position }) {
+    this.position = position;
+    this.width = Boundary.width;
+    this.height = Boundary.height;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = [];
+const offset = {
+  x: -249,
+  y: 13
+}
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x - 110,
+            y: i * Boundary.height + offset.y - 10,
+          },
+        })
+      );
+  });
+});
+//console.log(boundaries);
 
 const image = new Image();
 image.src = "./img/Petalwood Town.png"; //html element i.e. the image(map)
@@ -28,8 +68,8 @@ class Sprite {
 
 const background = new Sprite({
   position: {
-    x: -199,
-    y: 0,
+    x: offset.x,
+    y: offset.y,
   },
   image: image,
 });
@@ -53,6 +93,10 @@ function animate() {
   window.requestAnimationFrame(animate);
   background.draw();
 
+  boundaries.forEach(boundary => {
+    boundary.draw();
+  })
+
   // Scale the player image
   const playerWidth = playerImage.width * 0.8; // Scale down by 80% (adjust this value as needed)
   const playerHeight = playerImage.height * 0.8;
@@ -63,40 +107,40 @@ function animate() {
     0,
     playerImage.width / 4,
     playerImage.height,
-    canvas.width / 2 - playerImage.width / 4 / 1.26,
-    canvas.height / 2 - playerImage.height / 2 - 28,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
     playerWidth / 4, //actual height and width of player being rendered out
     playerHeight
   );
 
-  if (keys.w.pressed && lastkey === 'w' ) background.position.y += 1.5;
-  else if (keys.a.pressed && lastkey === 'a') background.position.x += 1.5;
-  else if (keys.s.pressed && lastkey === 's') background.position.y -= 1.5;
-  else if (keys.d.pressed && lastkey === 'd') background.position.x -= 1.5;
+  if (keys.w.pressed && lastkey === "w") background.position.y += 1.5;
+  else if (keys.a.pressed && lastkey === "a") background.position.x += 1.5;
+  else if (keys.s.pressed && lastkey === "s") background.position.y -= 1.5;
+  else if (keys.d.pressed && lastkey === "d") background.position.x -= 1.5;
 }
 animate();
 
-let lastkey = ''
+let lastkey = "";
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
       keys.w.pressed = true;
-      lastkey = 'w';
+      lastkey = "w";
       break;
 
     case "a":
       keys.a.pressed = true;
-      lastkey = 'a';
+      lastkey = "a";
       break;
 
     case "s":
       keys.s.pressed = true;
-      lastkey = 's';
+      lastkey = "s";
       break;
 
     case "d":
       keys.d.pressed = true;
-      lastkey = 'd';
+      lastkey = "d";
       break;
   }
 });
@@ -120,8 +164,8 @@ window.addEventListener("keyup", (e) => {
       break;
   }
 
-  if(keys.w.pressed) lastkey = 'w';
-  else if(keys.a.pressed) lastkey = 'a';
-  else if(keys.s.pressed) lastkey = 's';
-  else if(keys.d.pressed) lastkey = 'd';
+  if (keys.w.pressed) lastkey = "w";
+  else if (keys.a.pressed) lastkey = "a";
+  else if (keys.s.pressed) lastkey = "s";
+  else if (keys.d.pressed) lastkey = "d";
 });
